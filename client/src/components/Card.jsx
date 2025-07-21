92027bb7891fa1d6f2433c8ef1c173850c1da26b
Eyejangg <ptwptw1550@gmail.com>
2025-07-21 15:04:34 +0700

    update

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+//card
+const Card = (props) => {
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/v1/restaurants/" + id,
+        {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (response.ok) {
+        alert("Restaurant had been deleted!!");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img src={props.imageUrl} alt="IMAGE" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {props.name}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>{props.type}</p>
+        <div className="card-actions justify-end">
+          <a href={"/update/" + props.id} className="btn btn-warning">
+            Edit
+          </a>
+          <button
+            onClick={() => handleDelete(props.id)}
+            className="btn btn-error"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import restaurantRouter from "./routers/restaurant.router.js";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+import cors from "cors";
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "127.0.0.1:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Restaurant Restful API");
+});
+
+//use router
+app.use("/api/v1/restaurants", restaurantRouter);
+app.listen(PORT, () => {
+  console.log("Listening to http://localhost:" + PORT);
+});
